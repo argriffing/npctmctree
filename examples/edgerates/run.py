d5dbc9575f402e92a199c68934f48b650f190202
alex <argriffi@ncsu.edu>
2014-05-29 18:31:06 -0400

    add init file

--- examples/edgerates/run.py
+++ examples/edgerates/run.py
@@ -13,7 +13,10 @@ from scipy.linalg import expm, expm_frechet
 import npmctree
 from npmctree.puzzles import sample_distn1d
 #from npmctree.dynamic_fset_lhood import get_lhood, get_edge_to_distn2d
-from npmctree.cy_dynamic_lmap_lhood import get_lhood, get_edge_to_distn2d
+#from npmctree.cy_dynamic_lmap_lhood import get_lhood, get_edge_to_distn2d
+
+import npctmctree
+from npctmctree.cyem import expectation_step
 
 
 def get_tree_info():
@@ -39,75 +42,9 @@ def get_tree_info():
     return T, root, edge_to_rate
 
 
-def main():
-    np.random.seed(12345)
-
-    # Define the size of the state space
-    # which will be constant across the whole tree.
-    n = 4
-
-    # Sample a random root distribution as a 1d numpy array.
-    pzero = 0
-    root_distn1d = sample_distn1d(n, pzero)
-
-    # Hardcode a tree with four leaves
-    # and some arbitrary hardcoded rate scaling factors per edge.
-    T, root, edge_to_rate = get_tree_info()
-
-    # Sample a random rate matrix for each edge.
-    # These rate matrices have no redeeming qualities --
-    # they are not scaled in any particular way,
-    # they are not symmetric, they are not time-reversible,
-    # and their equilibrium distributions are unrelated
-    # to the root distribution.
-    edge_to_Q = {}
-    for edge in T.edges():
-        M = np.exp(np.random.randn(n, n))
-        Q = M - np.diag(M.sum(axis=1))
-        edge_to_Q[edge] = Q
-
-    # Convert edge-specific transition rate matrices
-    # to edge-specific transition probability matrices.
-    edge_to_P = {}
-    for edge in T.edges():
-        edge_rate = edge_to_rate[edge]
-        edge_Q = edge_to_Q[edge]
-        P = expm(edge_rate * edge_Q)
-        edge_to_P[edge] = P
-
-    # Instead of sampling states at the leaves,
-    # find the exact joint distribution of leaf states.
-    leaves = ('N0', 'N5', 'N3', 'N4')
-    internal_nodes = ('N1', 'N2')
-    states = range(n)
-    # Initialize the distribution over leaf data (yes this is confusing).
-    data_prob_pairs = []
-    for assignment in product(states, repeat=len(leaves)):
-
-        # Get the map from leaf to state.
-        leaf_to_state = dict(zip(leaves, assignment))
-
-        # Define the data associated with this assignment.
-        # All leaf states are fully observed.
-        # All internal states are completely unobserved.
-        node_to_data_fvec1d = {}
-        for node in leaves:
-            state = leaf_to_state[node]
-            fvec1d = np.zeros(n, dtype=bool)
-            fvec1d[state] = True
-            node_to_data_fvec1d[node] = fvec1d
-        for node in internal_nodes:
-            fvec1d = np.ones(n, dtype=bool)
-            node_to_data_fvec1d[node] = fvec1d
-
-        # Compute the likelihood for this data.
-        lhood = get_lhood(T, edge_to_P, root, root_distn1d, node_to_data_fvec1d)
-        data_prob_pairs.append((node_to_data_fvec1d, lhood))
-
-    # Check that the computed joint distribution over leaf states
-    # is actually a distribution.
-    datas, probs = zip(*data_prob_pairs)
-    assert_allclose(sum(probs), 1)
+def do_em(T, root, edge_to_rate, edge_to_Q, root_distn1d, data_prob_pairs):
+    # Extract the number of states.
+    n = root_distn1d.shape[0]
 
     # Try to guess the edge-specific scaling factors using EM,
     # starting with an initial guess that is wrong.
@@ -206,6 +143,80 @@ def main():
             #print(edge, trans, dwell, ratio, old_rate, new_rate)
             print(edge, new_rate)
         print()
+
+
+def main():
+    np.random.seed(12345)
+
+    # Define the size of the state space
+    # which will be constant across the whole tree.
+    n = 4
+
+    # Sample a random root distribution as a 1d numpy array.
+    pzero = 0
+    root_distn1d = sample_distn1d(n, pzero)
+
+    # Hardcode a tree with four leaves
+    # and some arbitrary hardcoded rate scaling factors per edge.
+    T, root, edge_to_rate = get_tree_info()
+
+    # Sample a random rate matrix for each edge.
+    # These rate matrices have no redeeming qualities --
+    # they are not scaled in any particular way,
+    # they are not symmetric, they are not time-reversible,
+    # and their equilibrium distributions are unrelated
+    # to the root distribution.
+    edge_to_Q = {}
+    for edge in T.edges():
+        M = np.exp(np.random.randn(n, n))
+        Q = M - np.diag(M.sum(axis=1))
+        edge_to_Q[edge] = Q
+
+    # Convert edge-specific transition rate matrices
+    # to edge-specific transition probability matrices.
+    edge_to_P = {}
+    for edge in T.edges():
+        edge_rate = edge_to_rate[edge]
+        edge_Q = edge_to_Q[edge]
+        P = expm(edge_rate * edge_Q)
+        edge_to_P[edge] = P
+
+    # Instead of sampling states at the leaves,
+    # find the exact joint distribution of leaf states.
+    leaves = ('N0', 'N5', 'N3', 'N4')
+    internal_nodes = ('N1', 'N2')
+    states = range(n)
+    # Initialize the distribution over leaf data (yes this is confusing).
+    data_prob_pairs = []
+    for assignment in product(states, repeat=len(leaves)):
+
+        # Get the map from leaf to state.
+        leaf_to_state = dict(zip(leaves, assignment))
+
+        # Define the data associated with this assignment.
+        # All leaf states are fully observed.
+        # All internal states are completely unobserved.
+        node_to_data_fvec1d = {}
+        for node in leaves:
+            state = leaf_to_state[node]
+            fvec1d = np.zeros(n, dtype=bool)
+            fvec1d[state] = True
+            node_to_data_fvec1d[node] = fvec1d
+        for node in internal_nodes:
+            fvec1d = np.ones(n, dtype=bool)
+            node_to_data_fvec1d[node] = fvec1d
+
+        # Compute the likelihood for this data.
+        lhood = get_lhood(T, edge_to_P, root, root_distn1d, node_to_data_fvec1d)
+        data_prob_pairs.append((node_to_data_fvec1d, lhood))
+
+    # Check that the computed joint distribution over leaf states
+    # is actually a distribution.
+    datas, probs = zip(*data_prob_pairs)
+    assert_allclose(sum(probs), 1)
+
+    do_em(T, root, edge_to_rate, edge_to_Q, root_distn1d, data_prob_pairs)
+
 
 
 if __name__ == '__main__':
