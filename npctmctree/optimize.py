77e076ad1f5ce873fe4e521003228e92a17c9c3d
alex <argriffi@ncsu.edu>
2014-06-10 15:07:10 -0400

    begin an optimization module

---
+++ npctmctree/optimize.py
@@ -0,0 +1,13 @@
+"""
+Maximum likelihood estimation.
+
+"""
+from __future__ import division, print_function, absolute_import
+
+import numpy as np
+
+from .cyem import expectation_step
+
+def estimate_edge_rates():
+    pass
+
