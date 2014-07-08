45a150d1c1db2d17905dce52a5f61d0b5c41bf33
alex <argriffi@ncsu.edu>
2014-07-08 11:46:24 -0400

    more work on EM inference example

--- examples/hky/check-inference.py
+++ examples/hky/check-inference.py
@@ -11,6 +11,17 @@ are computed using Monte Carlo.
 """
 from __future__ import division, print_function, absolute_import
 
+from itertools import product
+
+import numpy as np
+import networkx as nx
+from numpy.testing import assert_allclose
+from scipy.linalg import expm
+
+from npmctree import dynamic_fset_lhood
+
+from npctmctree import hkymodel
+
 
 def run_inference(T, root, bfs_edges, leaves,
         data_prob_pairs,
@@ -18,15 +29,28 @@ def run_inference(T, root, bfs_edges, leaves,
         ):
     """
     """
-    pass
+    return 0, 0, 0
 
 
-def get_data_prob_pairs(T, root, bfs_edges, leaves,
-        kappa, nt_probs, edge_rates):
+def get_hky_edge_to_P(T, root, bfs_edges, kappa, nt_probs, edge_rates):
     """
+    Get the per-edge probability transition matrices under the HKY model.
+
+    Compute the conditional transition probability matrices on edges,
+    under the given parameter values.
+    Use a careful interpretation of rate scaling.
+
+    Parameters
+    ----------
+    x : x
+        x
+
+    Returns
+    -------
+    x : x
+        x
+
     """
-    # Compute the conditional transition probability matrices on edges,
-    # under the given parameter values.
     pre_Q = hkymodel.get_pre_Q(kappa, nt_probs)
     rates_out = pre_Q.sum(axis=1)
     expected_rate = nt_probs.dot(rates_out)
@@ -34,37 +58,7 @@ def get_data_prob_pairs(T, root, bfs_edges, leaves,
     edge_to_P = {}
     for edge, edge_rate in zip(bfs_edges, edge_rates):
         edge_to_P[edge] = expm(edge_rate * Q)
-
-    # Compute the state distribution at the leaves,
-    # under the given parameter values.
-    states = range(n)
-    data_prob_pairs = []
-    for assignment in product(states, repeat=len(leaves)):
-
-        # Get the map from leaf to state.
-        leaf_to_state = dict(zip(leaves, assignment))
-
-        # Define the data associated with this assignment.
-        # All leaf states are fully observed.
-        # All internal states are completely unobserved.
-        node_to_data_fvec1d = {}
-        for node in leaves:
-            state = leaf_to_state[node]
-            fvec1d = np.zeros(n, dtype=bool)
-            fvec1d[state] = True
-            node_to_data_fvec1d[node] = fvec1d
-        for node in internal_nodes:
-            fvec1d = np.ones(n, dtype=bool)
-            node_to_data_fvec1d[node] = fvec1d
-
-        # Compute the likelihood for this data.
-        lhood = get_lhood(T, edge_to_P, root, root_distn1d, node_to_data_fvec1d)
-        data_prob_pairs.append((node_to_data_fvec1d, lhood))
-
-    # Check that the computed joint distribution over leaf states
-    # is actually a distribution.
-    datas, probs = zip(*data_prob_pairs)
-    assert_allclose(sum(probs), 1)
+    return edge_to_P
 
 
 def main():
@@ -92,10 +86,21 @@ def main():
     init_nt_probs = np.array([0.25, 0.25, 0.25, 0.25])
     init_edge_rates = np.array([0.2, 0.2, 0.2, 0.2, 0.2])
 
+    # Compute the map from edge to transition probability matrix,
+    # under the true parameter values.
+    edge_to_P = get_hky_edge_to_P(T, root, bfs_edges,
+            true_kappa, true_nt_probs, true_edge_rates)
+
     # Compute the state distribution at the leaves,
     # under the arbitrary 'true' parameter values.
-    data_prob_pairs = get_data_prob_pairs(T, root, bfs_edges, leaves,
-            kappa, nt_probs, edge_rates)
+    root_prior_distn1d = true_nt_probs
+    data_prob_pairs = dynamic_fset_lhood.get_unconditional_joint_distn(
+            T, edge_to_P, root, root_prior_distn1d, leaves)
+
+    # Check that the computed joint distribution over leaf states
+    # is actually a distribution.
+    sites, probs = zip(*data_prob_pairs)
+    assert_allclose(sum(probs), 1)
 
     # Check that the 'true' parameters can be inferred given
     # the 'true' state distribution at the leaves and arbitrary
